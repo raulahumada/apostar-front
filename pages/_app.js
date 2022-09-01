8b61af54081211b5fd49df8e60632590b75a0afc
rahumada <rahumada@galiciaseguros.com.ar>
2022-09-01 19:50:45 -0300

    Entrega inicial, instalación de nextjs y chakra

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+import '../styles/globals.css';
+
+const colors = {
+  apostar: {
+    black: '#000000',
+    red50: '#8f1414',
+    red100: '#e50e0e',
+    red150: '#f3450f',
+    white: '#ffffff',
+    gray: '#EDEEF7',
+    purple: '#7868E6',
+    purple50: '#B8B5FF',
+    turquoise: '#E4FBFF',
+  },
+};
+
+const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
